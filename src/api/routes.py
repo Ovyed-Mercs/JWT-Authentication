8e55ba8b97f91dc46ebcf31e353880de9610299b
OVYED Deno <nevad34@gmail.com>
2023-04-13 15:04:14 +0000

    4/13

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 
 api = Blueprint('api', __name__)
@@ -60,6 +58,18 @@ def login_user():
     user= User.query.filter_by(email=email, password=password).first()
     
     if user is None:
-        return jsonify({"message":"bad username or password"}), 401
+        return jsonify({"message":"bad email or password"}), 401
     
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def get_private():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    response_body={
+        "message":"congrats",
+        "user": current_user
+    }
     return jsonify(response_body), 200
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,51 +1,33 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	let apiUrl=process.env.BACKEND_URL
 	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
+		store: {},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			signin:()=>{
+				let requestOptions = {
+					method: 'POST',
+					headers: {"Content-Type": "application/json"},
+					body: {email:"email",
+					password:"password"},
+				  };
+				  
+				  fetch(apiUrl+"/api/login", requestOptions)
+					.then(response => response.text())
+					.then(result => console.log(result))
+					.catch(error => console.log('error', error));
 			},
+			access_private: ()=>{
+				let requestOptions = {
+				method: 'GET',
+				headers: {
+					"Authorization": "Bearer "+localStorage.getItem("token"),
+					"Content-Type": "application/json"
+				}};
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				fetch(apiUrl+"/api/private", requestOptions)
+				.then(response => response.json())
+				.then(result => console.log(result))
+				.catch(error => console.log('error', error));
 			}
 		}
 	};
